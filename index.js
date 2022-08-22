1f038e1df9149ce66b800f2364cbc06acf27db18
bruteforce-man <puneetpatientbond@gmail.com>
2022-08-22 13:18:34 +0530

    feat : added day

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-/* TODO: add module */
+/* TODO: add timezone support*/
+const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let timezone;
+
+
+
+
+function Today = function(options){
+	if(options.timeZone){
+	timezone =  options.timezone;
+	}
+
+}
+
+Today.prototype.isToday = (day) => {
+  if(!day){
+  return false
+  }
+  if (typeof day === "string"){
+    return DAYS[Date.getDay()].toUpperCase() === day.toUpperCase();
+   } else {
+      return DAYS[Date.getDay()] === day;
+   }	
+}
+
+
+Today.prototype.currentDay = () =>{
+  return DAYS[Date.getDay()];
+}
+
+
+module.exports = Today;
